1a02ff5f4e3eac4c23b222e465c66fa1f374659e
Mustafa <mustafakoceerr@hotmail.com>
2025-05-28 16:46:49 +0300

    Splash screen added

---
+++ app/src/main/java/com/mustafakocer/harcamabolustur/presentation/splash/SplashScreen.kt
@@ -0,0 +1,79 @@
+package com.mustafakocer.harcamabolustur.presentation.splash
+
+import android.window.SplashScreen
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mustafakocer.harcamabolustur.ui.theme.HarcamaBolusturTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    onNavigateToAuth: () -> Unit,
+    onNavigateToMain: () -> Unit,
+) {
+// TODO: Auth State kontrolü yapılacak.
+    // şimdilik 2 saniye bekleyip auth'a yönlendir.
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onNavigateToAuth() // şimdilik hep auth'a git.
+    }
+
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            // Logo placeholder
+            Text(
+                text = "💰",
+                fontSize = 64.sp
+            )
+
+            Text(
+                text = "DebtSync",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = "Sync your expenses, settle your debts",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            CircularProgressIndicator(
+                modifier = Modifier.size(32.dp),
+                strokeWidth = 3.dp
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    HarcamaBolusturTheme {
+        SplashScreen(
+            onNavigateToAuth = {},
+            onNavigateToMain = {}
+        )
+    }
+
+}
