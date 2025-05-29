d09a0c6f916a90d1ebc06270759a19cbaa8c5366
Mustafa <mustafakoceerr@hotmail.com>
2025-05-29 15:35:47 +0300

    Hilt annotation added to MainActivity.kt

--- app/src/main/java/com/mustafakocer/harcamabolustur/MainActivity.kt
+++ app/src/main/java/com/mustafakocer/harcamabolustur/MainActivity.kt
@@ -14,6 +14,7 @@ import com.mustafakocer.harcamabolustur.presentation.navigation.DebtSyncNavigati
 import com.mustafakocer.harcamabolustur.ui.theme.DebtSyncTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
