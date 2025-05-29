fc4da730585183f904b69b0b4b0a249c0a17e7e8
Mustafa <mustafakoceerr@hotmail.com>
2025-05-29 12:21:50 +0300

    DatabaseConstants.kt added

---
+++ app/src/main/java/com/mustafakocer/harcamabolustur/core/constants/DatabaseConstants.kt
@@ -0,0 +1,12 @@
+package com.mustafakocer.harcamabolustur.core.constants
+
+object DatabaseConstants {
+    const val DATABASE_NAME = "debt_sync_database"
+    const val DATABASE_VERSION = 1
+
+    // Entity Types for RemoteKeys
+    const val ENTITY_TYPE_GROUPS = "groups"
+    const val ENTITY_TYPE_EXPENSES = "expenses"
+    const val ENTITY_TYPE_DEBTS = "debts"
+    const val ENTITY_TYPE_USERS = "users"
+}
